e5297a47fbfee542968664d50778018d83683e28
Angel Quintero <angelqv2023@gmail.com>
2024-08-20 22:34:56 -0400

    Update Useful C++ funtions.cpp

--- Useful C++ funtions.cpp
+++ Useful C++ funtions.cpp
@@ -7,3 +7,13 @@ bool CheckStringToInt(string string)
         else return true;
     }
 }
+
+//Check if a number id odd or even
+if (number % 2 == 0)
+{
+    result = "even";
+}
+else 
+{
+    result = "odd";
+}
