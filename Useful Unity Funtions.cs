30b5171a9ad779a24d273e74948554e24d2c5bb3
Angel Quintero <angelqv2023@gmail.com>
2024-08-22 22:20:54 -0400

    Create Useful Unity Funtions.cs

---
+++ Useful Unity Funtions.cs
@@ -0,0 +1,6 @@
+///Used to determine if a string can be safely converted into an int. outs the result if it does.
+if (!int.TryParse(textAsString, out numberAsInt))
+{
+  Debug.Log("Error. Please insert numbers only.");
+  return;
+}
